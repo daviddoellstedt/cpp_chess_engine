2ccf26c67303449c26cf41044ec79e888188d41f
daviddoellstedt <daviddoellstedt@gmail.com>
2024-05-31 01:29:35 -0700

    made fen reader more robust

--- bit_fns.cpp
+++ bit_fns.cpp
@@ -12,6 +12,7 @@
 #include <ctime>
 #include <iostream>
 #include <map>
+#include <regex>
 #include <stdint.h>
 #include <string>
 #include <vector>
@@ -124,15 +125,7 @@ uint8_t findSetBit(uint64_t bitboard) {
  * arguments: the 12 bitboards for the all the pieces
  */
 void print_board(const GameState gamestate) {
-  char grid[8][8] = {// 0 |  1 |  2 |  3 |  4 |  5 |  6 |  7 |
-                     {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},  // 7
-                     {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},  // 6
-                     {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},  // 5
-                     {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},  // 4
-                     {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},  // 3
-                     {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},  // 2
-                     {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},  // 1
-                     {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}}; // 0
+  char grid[8][8];
 
   for (int8_t row = 7; row >= 0; row--) {
     for (uint8_t col = 0; col < 8; col++) {
@@ -161,6 +154,8 @@ void print_board(const GameState gamestate) {
         grid[row][col] = 'q';
       } else if (gamestate.black.king & (1ULL << bit)) {
         grid[row][col] = 'k';
+      } else {
+        grid[row][col] = ' ';
       }
     }
   }
@@ -1869,98 +1864,181 @@ AI_return minimax(GameState &gamestate, uint64_t E_P, bool CM, bool SM,
 }
 
 void fenToGameState(const std::string fen, GameState &gamestate) {
+
+  std::string regex_string =
+      "^ *([rnbqkpRNBQKP1-8]+/){7}([rnbqkpRNBQKP1-8]+) ([wb]) "
+      "(([kqKQ]{1,4})|(-)) (([a-h][36])|(-)) *(.*)$";
+
+  if (!regex_match(fen, std::regex(regex_string))) {
+    logErrorAndExit("ERROR: Not a valid FEN.");
+    return;
+  }
+
+  // Counters to validate FEN is a legal and possible position.
+  // TODO: validate the castling flags and rook/king location.
+  // TODO: make sure kings are separated by at least 1 square.
+  // TODO: the sum of the pieces and empty squares sum to 8 per rank.
+  // TODO: there should be no consecutive numbers.
+  // TODO: non active color is not in check.
+  // For more info:
+  // https://chess.stackexchange.com/questions/1482/how-do-you-know-when-a-fen-position-is-legal
+
+  uint8_t n_p = 0;
+  uint8_t n_r = 0;
+  uint8_t n_n = 0;
+  uint8_t n_b = 0;
+  uint8_t n_q = 0;
+  uint8_t n_k = 0;
+  uint8_t n_P = 0;
+  uint8_t n_R = 0;
+  uint8_t n_N = 0;
+  uint8_t n_B = 0;
+  uint8_t n_Q = 0;
+  uint8_t n_K = 0;
+
   uint8_t byte = 7;
   uint8_t bit = 0;
 
   // Populate piece positions.
-  for (int i = 0; i < fen.find(' '); i++) {
+  for (uint8_t i = 0; i < fen.find(' '); i++) {
     // Check for empty positions.
     if (isdigit(fen[i])) {
       bit += fen[i] - '0';
+      continue;
     }
     switch (fen[i]) {
     case 'p':
+      if (byte == 0 || byte == 7) {
+        logErrorAndExit("ERROR: Pawns cannot be on rank 1 or rank 8.");
+        memset(&gamestate, 0, sizeof(GameState));
+        return;
+      }
       gamestate.black.pawn += 1ull << (byte * 8 + bit);
-      bit++;
+      n_p++;
       break;
     case 'r':
       gamestate.black.rook += (1ull << (byte * 8 + bit));
-      bit++;
+      n_r++;
       break;
     case 'n':
       gamestate.black.knight += 1ull << (byte * 8 + bit);
-      bit++;
+      n_n++;
       break;
     case 'b':
       gamestate.black.bishop += 1ull << (byte * 8 + bit);
-      bit++;
+      n_b++;
       break;
     case 'q':
       gamestate.black.queen += 1ull << (byte * 8 + bit);
-      bit++;
+      n_q++;
       break;
     case 'k':
       gamestate.black.king += 1ull << (byte * 8 + bit);
-      bit++;
+      n_k++;
       break;
     case 'P':
+      if (byte == 0 || byte == 7) {
+        logErrorAndExit("ERROR: Pawns cannot be on rank 1 or rank 8.");
+        memset(&gamestate, 0, sizeof(GameState));
+        return;
+      }
       gamestate.white.pawn += 1ull << (byte * 8 + bit);
-      bit++;
+      n_P++;
       break;
     case 'R':
       gamestate.white.rook += 1ull << (byte * 8 + bit);
-      bit++;
+      n_R++;
       break;
     case 'N':
       gamestate.white.knight += 1ull << (byte * 8 + bit);
-      bit++;
+      n_N++;
       break;
     case 'B':
       gamestate.white.bishop += 1ull << (byte * 8 + bit);
-      bit++;
+      n_B++;
       break;
     case 'Q':
       gamestate.white.queen += 1ull << (byte * 8 + bit);
-      bit++;
+      n_Q++;
       break;
     case 'K':
       gamestate.white.king += 1ull << (byte * 8 + bit);
-      bit++;
+      n_K++;
       break;
     case '/':
       byte -= 1;
       bit = 0;
-      break;
+      continue;
     default:
-      // todo: add error catch here.
+      logErrorAndExit("ERROR: Unknown character in FEN string!");
       break;
     }
+    bit++;
+  }
+
+  // Piece count sanity checks.
+  int8_t n_p_missing = 8 - n_p;
+  int8_t n_P_missing = 8 - n_P;
+  uint8_t n_promoted = (n_r > 2 ? n_r - 2 : 0) + (n_n > 2 ? n_n - 2 : 0) +
+                       (n_b > 2 ? n_b - 2 : 0) + (n_q > 1 ? n_q - 1 : 0);
+  uint8_t n_Promoted = (n_R > 2 ? n_R - 2 : 0) + (n_N > 2 ? n_N - 2 : 0) +
+                       (n_B > 2 ? n_B - 2 : 0) + (n_Q > 1 ? n_Q - 1 : 0);
+  if (n_P > 8 || n_p > 8 || n_K != 1 || n_k != 1 || n_p_missing < n_promoted ||
+      n_P_missing < n_Promoted) {
+    logErrorAndExit("ERROR: FEN position not legal/possible.");
+    memset(&gamestate, 0, sizeof(GameState));
+    return;
   }
 
   // Populate extra game state data.
-  for (int i = fen.find(' ') + 1; i < fen.length(); i++) {
-    switch (fen[i]) {
-    case 'w':
-      gamestate.whites_turn = true;
-      break;
-    case 'b':
-      gamestate.whites_turn = false;
-      break;
-    case 'K':
-      gamestate.white.can_king_side_castle = true;
-      break;
-    case 'Q':
-      gamestate.white.can_queen_side_castle = true;
-      break;
-    case 'k':
-      gamestate.black.can_king_side_castle = true;
-      break;
-    case 'q':
-      gamestate.black.can_queen_side_castle = true;
-      break;
-    default:
-      // todo: add error checking here.
-      break;
+  // TODO: Add half move and full move functionality. The 4th and 5th fields.
+  uint8_t field = 1; // 1: turn, 2: castling flags, 3: en passant.
+  for (uint8_t i = fen.find(' ') + 1; i < fen.length(); i++) {
+    // Nit: could be a switch statement.
+    if (field == 1) {
+      if (fen[i] == 'w') {
+        gamestate.whites_turn = true;
+        field++;
+        continue;
+      }
+      if (fen[i] == 'b') {
+        gamestate.whites_turn = false;
+        field++;
+        continue;
+      }
+    } else if (field == 2) {
+      if (((gamestate.white.can_king_side_castle ||
+            gamestate.white.can_queen_side_castle ||
+            gamestate.black.can_king_side_castle ||
+            gamestate.black.can_queen_side_castle) &&
+           fen[i] == ' ') ||
+          fen[i] == '-') {
+        field++;
+        continue;
+      }
+      if (fen[i] == 'K') {
+        gamestate.white.can_king_side_castle = true;
+        continue;
+      }
+      if (fen[i] == 'Q') {
+        gamestate.white.can_queen_side_castle = true;
+        continue;
+      }
+      if (fen[i] == 'k') {
+        gamestate.black.can_king_side_castle = true;
+        continue;
+      }
+      if (fen[i] == 'q') {
+        gamestate.black.can_queen_side_castle = true;
+        continue;
+      }
+    } else {
+      if (fen[i] >= 'a' && fen[i] <= 'h') {
+        uint8_t col = fen[i] - 'a';
+        uint8_t row = fen[i + 1] - '0' - 1;
+        // gamestate.en_passant = 1ull << (row * 8 + col);
+        // std::cout<< (row * 8 + col) << std::endl;
+      }
     }
   }
 }
