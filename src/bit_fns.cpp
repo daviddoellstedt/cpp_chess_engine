86e5cc0d2b5b5fd9062b6d4c4e7e27e5cec453df
daviddoellstedt <daviddoellstedt@gmail.com>
2024-06-14 17:36:01 -0700

    much improved evaluation function

--- src/bit_fns.cpp
+++ src/bit_fns.cpp
@@ -12,25 +12,53 @@
 #include <stdint.h>
 #include <string>
 
-int16_t eval(const GameState game_state) {
-  // material
+// TODO documentation
+int16_t getIndividualBitboardScoreMaterialAndPosition(
+    uint64_t bitboard_w, uint64_t bitboard_b,
+    const int8_t position_adjustment[64], const uint16_t material_value) {
   int16_t counter = 0;
-  counter += (countSetBits(game_state.white.pawn) -
-              countSetBits(game_state.black.pawn)) *
-             100;
-  counter += (countSetBits(game_state.white.bishop) -
-              countSetBits(game_state.black.bishop)) *
-             300; // todo: add special case regarding number of bishops
-  counter += (countSetBits(game_state.white.knight) -
-              countSetBits(game_state.black.knight)) *
-             300;
-  counter += (countSetBits(game_state.white.rook) -
-              countSetBits(game_state.black.rook)) *
-             500;
-  counter += (countSetBits(game_state.white.queen) -
-              countSetBits(game_state.black.queen)) *
-             900;
+  while (bitboard_w) {
+    counter += material_value;
+    counter += position_adjustment[getSetBit(getLowestSetBitValue(bitboard_w))];
+    clearLowestSetBit(bitboard_w);
+  }
+
+  while (bitboard_b) {
+    counter -= material_value;
+    counter -=
+        position_adjustment[63 - getSetBit(getLowestSetBitValue(bitboard_b))];
+    clearLowestSetBit(bitboard_b);
+  }
   return counter;
+}
+
+// TODO documentation
+int16_t getScoreMaterialAndPosition(GameState game_state) {
+  int16_t counter = 0;
+  counter += getIndividualBitboardScoreMaterialAndPosition(
+      game_state.white.pawn, game_state.white.pawn, pawn_position_adjustment,
+      PAWN_VALUE);
+  counter += getIndividualBitboardScoreMaterialAndPosition(
+      game_state.white.knight, game_state.white.knight,
+      knight_position_adjustment, KNIGHT_VALUE);
+  counter += getIndividualBitboardScoreMaterialAndPosition(
+      game_state.white.bishop, game_state.white.bishop,
+      bishop_position_adjustment, BISHOP_VALUE);
+  counter += getIndividualBitboardScoreMaterialAndPosition(
+      game_state.white.rook, game_state.white.rook, rook_position_adjustment,
+      ROOK_VALUE);
+  counter += getIndividualBitboardScoreMaterialAndPosition(
+      game_state.white.queen, game_state.white.queen, queen_position_adjustment,
+      QUEEN_VALUE);
+  counter += getIndividualBitboardScoreMaterialAndPosition(
+      game_state.white.king, game_state.white.king, king_position_adjustment,
+      0);
+  return counter;
+}
+
+// TODO documentation
+int16_t eval(const GameState game_state) {
+  return getScoreMaterialAndPosition(game_state);
 }
 
 AI_return negamax(GameState game_state, uint8_t depth, int8_t color,
